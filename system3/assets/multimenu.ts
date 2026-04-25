611f34fd09e4b457245faf896db0c7f92c2bdc7a
RonNewcomb <pscion@yahoo.com>
2026-04-25 16:57:47 -0700

    setting element focus cancels its entry animation. OK.

--- system3/assets/multimenu.ts
+++ system3/assets/multimenu.ts
@@ -35,7 +35,8 @@ export async function multimenu(topMenu: MenuElement): Promise<Result> {
     onSwipe = function (n: number) {
       currentSlide = Math.max(0, Math.min(currentSlide + n, slidingWindow.childElementCount - 1));
       slidingWindow.style.left = `calc(-${currentSlide * 100}% - ${currentSlide * 2}em)`; // 2em is the flex-gap
-      (slidingWindow.children[currentSlide]?.firstElementChild as HTMLElement)?.focus();
+      const setFocus = () => (slidingWindow.children[currentSlide]?.firstElementChild as HTMLElement)?.focus();
+      currentSlide ? setTimeout(setFocus, 750) : setFocus();
     };
     onSwipe(currentSlide); // init
 

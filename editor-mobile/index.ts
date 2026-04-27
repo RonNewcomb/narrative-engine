0c847272b99fd584e5b7495d932a5c80042116f6
RonNewcomb <pscion@yahoo.com>
2026-04-26 20:05:17 -0700

    speech doesnt "sustain" on mobile, on desktop i get multiple copies

--- editor-mobile/index.ts
+++ editor-mobile/index.ts
@@ -7,15 +7,17 @@ const startButton = document.getElementById("startButton")!;
 
 const handlers = speechToText({
   outputDiv: document.getElementById("output")!,
-  onStart: () => (startButton.textContent = "Listening..."),
-  onStop: () => (startButton.textContent = "Listen."),
+  onStart: () => {
+    startButton.classList.add("pulse");
+    startButton.textContent = "Listening...";
+  },
+  onStop: () => {
+    startButton.classList.remove("pulse");
+    startButton.textContent = "Listen.";
+  },
 });
 if (handlers) {
-  startButton!.addEventListener("click", e => {
-    const state = handlers.onToggle();
-    if (state) startButton.classList.add("pulse");
-    else startButton.classList.remove("pulse");
-  });
+  startButton!.addEventListener("click", handlers.onToggle);
   document.getElementById("shareButton")!.addEventListener("click", handlers.onShare);
   document.getElementById("copyButton")!.addEventListener("click", handlers.onCopyToClipboard);
 }
--- editor-mobile/speech-to-text.ts
+++ editor-mobile/speech-to-text.ts
@@ -1,7 +1,7 @@
 export interface ISpeechToText {
   outputDiv: HTMLElement;
-  onStart: ((this: SpeechRecognition, ev: Event) => any) | null;
-  onStop: ((this: SpeechRecognition, ev: Event) => any) | null;
+  onStart?: () => void;
+  onStop?: () => void;
 }
 
 export interface ReturnSpeechToText {
@@ -10,9 +10,11 @@ export interface ReturnSpeechToText {
   onCopyToClipboard: () => void;
 }
 
+let usingLocal = false;
+
 export function speechToText({ outputDiv, onStart, onStop }: ISpeechToText): ReturnSpeechToText | void {
   if (location.protocol !== "https:") return window.alert("Speech-to-text requires HTTPS protocol");
-  let toggle = false;
+  let isListening = false;
 
   const SR =
     window.SpeechRecognition ||
@@ -22,11 +24,20 @@ export function speechToText({ outputDiv, onStart, onStop }: ISpeechToText): Ret
   if (!SR) return window.alert("This browser does not support speech-to-text");
 
   const recognition = new SR();
-  recognition.lang = "en-US";
+  recognition.lang = navigator.language;
   recognition.continuous = true;
+  recognition.processLocally = usingLocal;
   recognition.onstart = onStart || (() => 0);
   recognition.onend = onStop || (() => 0);
   recognition.onresult = onEmit;
+  recognition.onend = () => onError(void 0);
+  recognition.onerror = onError;
+
+  function onError(e?: SpeechRecognitionErrorEvent) {
+    if (e) console.error("Speech recognition error", e);
+    isListening = false;
+    onStop?.();
+  }
 
   function onEmit(event: SpeechRecognitionEvent) {
     const transcript = event.results[0][0].transcript;
@@ -35,17 +46,16 @@ export function speechToText({ outputDiv, onStart, onStop }: ISpeechToText): Ret
   }
 
   function onToggle() {
-    if (!toggle) recognition.start();
-    else recognition.stop();
-    toggle = !toggle;
-    return toggle;
+    isListening ? recognition.stop() : recognition.start();
+    isListening = !isListening;
+    return isListening;
   }
 
   function onShare() {
     if (!navigator.share) return;
     navigator
       .share({
-        title: "Voice to Text",
+        title: document.title || "Speech to Text",
         text: outputDiv.textContent,
       })
       .then(() => console.log("Successful share"))
@@ -63,4 +73,21 @@ export function speechToText({ outputDiv, onStart, onStop }: ISpeechToText): Ret
   };
 }
 
+export function useLocalRecognition(local: boolean = true): Promise<string | undefined> | string | undefined {
+  usingLocal = local;
+  if (!usingLocal) return "";
+  const lang = navigator.language;
+  const options: SpeechRecognitionOptions = { langs: [lang], processLocally: true };
+  return SpeechRecognition.available(options).then(result => {
+    if (result === "unavailable") return `${lang} is not available to download at this time. Sorry!`;
+    if (result === "available") return undefined;
+    console.log(`${lang} language pack is downloading...`);
+    return SpeechRecognition.install(options).then(result => {
+      if (!result) return `${lang} language pack failed to download. Try again later.`;
+      return `${lang} language pack downloaded. Start recognition again.`;
+    });
+  });
+}
+
 (window as any).speechToText = speechToText;
+(window as any).useLocalRecognition = useLocalRecognition;
